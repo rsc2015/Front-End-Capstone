2b3e3f37c5d15781c42294a09118c61dbfa95fc7
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-03-19 19:45:47 -0500

    created js

---
+++ js/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+console.log("main.js");
+let $ = require('jquery');
+
+$(function () {
+    $('[data-toggle="popover"]').popover()({
+    placement : 'top',
+        trigger : 'hover'
+    });
+  });
+
+  $(".pop").popover({ trigger: "manual" , html: true, animation:false})
+    .on("mouseenter", function () {
+        var _this = this;
+        $(this).popover("show");
+        $(".popover").on("mouseleave", function () {
+            $(_this).popover('hide');
+        });
+    }).on("mouseleave", function () {
+        var _this = this;
+        setTimeout(function () {
+            if (!$(".popover:hover").length) {
+                $(_this).popover("hide");
+            }
+        }, 300);
+});
