ea5096fad9e399a2a60559b0171efe8058a0db97
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-20 22:28:59 -0500

    log-in/log-out functions created

--- js/main.js
+++ js/main.js
@@ -6,6 +6,28 @@ window.Popper = require('popper.js');
 let db = require("./db-interaction");
 let templates = require("./dom-builder");
 let user = require("./user");
+let login = require("./user");
+
+$("#auth-btn").click(function(){
+    console.log("clicked on Signin");
+    
+    login.logInGoogle()
+    .then((result) => {
+      console.log("result from login", result.user.uid);
+      $("#auth-btn").hide();
+      $("#logout").show();
+      login.setUser(result.user.uid);
+  
+    });
+  });
+  
+  $("#logout").click(function(){
+    console.log("logout clicked");
+    $("#logout").hide();
+    $("#auth-btn").show();
+    login.logOut();
+  
+  });
 
  
 // $(document).ready(function(){
