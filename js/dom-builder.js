50abf70a3c6359e75d58727f6cc676fab0d0bb35
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-21 08:51:06 -0500

    corrected input typo

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -10,9 +10,17 @@ function printListToDom(symptomsList){
         let symptomNames = symptomsList.symptoms;
         console.log("Symptom Name:", symptomNames[i].name);
         $('#symptomData').append(`<li class="list-group-item symptomsDisplay" name="symName">
-        ${symptomNames[i].name}<input class="single-checkbox"type="checkbox" name="test" value="1" /></li>`);
+        ${symptomNames[i].name}<input class="singlecheckbox" type="checkbox" name="test" value="1" /></li>`);
     }
     }
+
+    var input;
+
+    $('.singlecheckbox').on('change', function() {
+        if($('.singlecheckbox:checked').length > 2) {
+            input.checked = false;
+        }
+     });
 
 
 module.exports = {printListToDom};
