fd4592f3639f403ca6d6a61319d95c3a46487c49
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-20 22:32:30 -0500

    removed useless functions/comments

--- js/main.js
+++ js/main.js
@@ -30,35 +30,3 @@ $("#auth-btn").click(function(){
   });
 
  
-// $(document).ready(function(){
-//     $("#selectSymptom").click(function(){
-//         $("#exampleModalLong").modal();
-//     });
-// });
-
-// $(document).ready(function(){
-//     $(".dropdown-toggle").dropdown();
-//   });
-
-// $(function () {
-//     $('[data-toggle="popover"]').popover()({
-//     placement : 'top',
-//         trigger : 'hover'
-//     });
-//   });
-
-//   $(".pop").popover({ trigger: "manual" , html: true, animation:false})
-//     .on("mouseenter", function () {
-//         var _this = this;
-//         $(this).popover("show");
-//         $(".popover").on("mouseleave", function () {
-//             $(_this).popover('hide');
-//         });
-//     }).on("mouseleave", function () {
-//         var _this = this;
-//         setTimeout(function () {
-//             if (!$(".popover:hover").length) {
-//                 $(_this).popover("hide");
-//             }
-//         }, 300);
-// });
