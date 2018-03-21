82f90af29862b1a7e25ddb83ee5fc18ee5861d74
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-21 08:56:43 -0500

    created functions foruser log-in/log-out

--- js/user.js
+++ js/user.js
@@ -1,8 +1,37 @@
 "use strict";
 //install firebase into lib folder npm install firebase --save
 let firebase = require("./fb-config"),
-	//  provider = new firebase.auth.GoogleAuthProvider(),
+ provider = new firebase.auth.GoogleAuthProvider(),
 	 currentUser = null;
+
+
+function logInGoogle() {
+  return firebase.auth().signInWithPopup(provider);
+}
+
+function logOut(){
+  return firebase.auth().signOut();
+}
+
+function setUser(val){
+	currentUser = val;
+}
+
+function getUser() {
+  return currentUser;
+}
+
+firebase.auth().onAuthStateChanged(function(user){
+	console.log("onAuthStateChanged", user);
+	if (user){
+		currentUser = user.uid;
+	}else{
+		currentUser = null;
+		console.log("NO USER LOGGED IN");
+	}
+});
+
+module.exports = {logInGoogle, logOut, setUser, getUser};
 
 //listen for changed state
 // firebase.auth().onAuthStateChanged((user) => {
