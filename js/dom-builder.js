230f7a507dbf08007d89706f8fde636ab1ae5968
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-22 01:16:06 -0500

    changed symptoms to 12

--- js/dom-builder.js
+++ js/dom-builder.js
@@ -6,7 +6,7 @@ let $ = require('jquery');
 function printListToDom(symptomsList){
     console.log("symptomsList", symptomsList);
     // let eventsArray = meetupList.events;
-    for (var i=0; i < 10; i++){
+    for (var i=0; i < 12; i++){
         let symptomNames = symptomsList.symptoms;
         console.log("Symptom Name:", symptomNames[i].name);
         $('#symptomData').append(`<li class="list-group-item symptomsDisplay" name="symName">
