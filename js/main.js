59e224f37fd0e542d3d31410e4aa7936a19203ee
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-26 12:35:40 -0500

    created user object, login, logout

--- js/main.js
+++ js/main.js
@@ -2,31 +2,75 @@
 
 console.log("main.js");
 let $ = require('jquery');
-
 let db = require("./db-interaction");
+let build = require('./buildFBObj');
 let templates = require("./dom-builder");
 let user = require("./user");
 let login = require("./user");
+let fbConfig = require('./fb-config');
 
-$("#auth-btn").click(function(){
-    console.log("clicked on Signin");
+
+
+function createUserObj(fan) {
+  let userObj = {
+      name: '',
+      email: '',
+      uid: user.getUser()
+  };
+  return userObj;
+}
+var userID = "";
+
+//------- When user clicks login --------//
+$("#login").click(function() {
+  user.googlelogIn()
+  .then((result) => {
+      user.setUser(result.user.uid);
+      $("#login").hide();
+   $("#log-out").show();
+      $("#userPic").removeClass("no-user").html(`<img src="${result.user.photoURL}" alt="${result.user.displayName} photo from Google" class="profPic rounded-circle" width="50">`);
+      sendToFirebase();
+  });
+});
+
+
+
+//-------- Send user info to Firebase --------//
+
+function sendToFirebase() {
+  let userBuilder = createUserObj();
+  db.addUserFB(userBuilder);
+}
+
+$("#log-out").click(function(){
+  user.googleLogOut();
+  // $("#login").removeClass("is-hidden");
+  // $("#log-out").addClass("is-hidden");
+  $("#log-out").hide();
+  $("#login").show();
+});
+
+
+// $("#auth-btn").click(function(){
+//     console.log("clicked on Signin");
     
-    login.logInGoogle()
-    .then((result) => {
-      console.log("result from login", result.user.uid);
-      $("#auth-btn").hide();
-      $("#logout").show();
-      login.setUser(result.user.uid);
+//     login.logInGoogle()
+//     .then((result) => {
+//       console.log("result from login", result.user.uid);
+//       $("#auth-btn").hide();
+//       $("#logout").show();
+//       login.setUser(result.user.uid);
+//     });
+//   });
   
-    });
-  });
+//   $("#logout").click(function(){
+//     console.log("logout clicked");
+//     $("#logout").hide();
+//     $("#auth-btn").show();
+//     login.logOut();
   
-  $("#logout").click(function(){
-    console.log("logout clicked");
-    $("#logout").hide();
-    $("#auth-btn").show();
-    login.logOut();
-  
-  });
+//   });
+
+ 
 
  
