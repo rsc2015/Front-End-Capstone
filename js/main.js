0c680416b1223bc32ac4d8f26295847f6854625c
ROHINI CHAVAN <rohinichavan@gmail.com>
2018-03-26 22:44:35 -0500

    loadHistoryToView, saveBtn, editBtn, deleteBtn, BuildHistory, createUser

--- js/main.js
+++ js/main.js
@@ -9,9 +9,111 @@ let user = require("./user");
 let login = require("./user");
 let fbConfig = require('./fb-config');
 
+function loadHistoryToView() {
+  console.log("Need to load some history");
+  let currentUser = user.getUser(); //add once we have login
+  console.log("currentUser in loadHistory", currentUser);
+  db.getHistories(currentUser)
+  // db.getSongs()
+  .then((historyData) => {
+    console.log("got data", historyData);
+    //with users, this is already happening...
+    //add the id to each song and then build the song list
+    // var idArray = Object.keys(songData);
+    // idArray.forEach((key) => {
+    //   songData[key].id = key;
+    // });
+    // console.log("song object with id", songData);
+    //now make the list with songData
+    templates.createHistoryFormList(historyData);
+  });
+}
+
+// Send newhistory data to db then reload DOM with updated history data
+$(document).on("click", ".save_new_btn", function() {
+  console.log("click save new song");
+  let historyObj = buildHistoryObj();
+  db.addHistory(historyObj)
+  .then((historyID) => {
+    loadHistoryToView();
+  });
+});
+
+// go get the history from database and then populate the form for editing.
+$(document).on("click", ".edit-btn", function () {
+  console.log("click edit history");
+  let historyID = $(this).data("edit-id");
+  db.getHistory(historyID)
+  .then((history) => {
+    return templates.historyForm(history, historyID);
+  })
+  .then((finishedForm) => {
+    $(".uiContainer--wrapper").html(finishedForm);
+  });
+});
+
+//Save edited history to FB then reload DOM with updated history data
+$(document).on("click", ".save_edit_btn", function() {
+  let historyObj = buildHistoryObj(),
+  historyID = $(this).attr("id");
+  console.log("historyID", historyID);
+  db.editSong(historyObj, historyID)
+  .then((data) => {
+    loadHistoryToView();
+  });
+});
+
+// Remove history then reload the DOM w/out new history
+$(document).on("click", ".delete-btn", function () {
+  console.log("clicked delete history", $(this).data("delete-id"));
+  let historyID = $(this).data("delete-id");
+  db.deleteHistory(historyID)
+  .then(() => {
+    loadHistoryToView();
+  });
+});
+
+//make the view button work.
+//not needed when user logs in since that will
+//handle showing songs
+$("#view-historys").click(function() {
+  $(".uiContainer--wrapper").html("");
+  loadHistoryToView();
+});
 
 
-function createUserObj(fan) {
+//***************************************************************************** */
+// Helper functions for forms stuff. Nothing related to Firebase
+// Build a history obj from form data.
+
+// Helper functions for forms stuff. Nothing related to Firebase
+// Build a song obj from form data.
+// put into own module
+function buildHistoryObj() {
+  let historyObj = {
+  title: $("#form-date").val(),
+  med1: $("#form-med1").val(),
+  med2: $("#form-med2").val(),
+  med3: $("#form-med3").val(),
+  uid: user.getUser() // include uid to the object only if a user is logged in.
+};
+return historyObj;
+}
+function createUserHistory(history) {
+  let userHistory = {
+      name: '',
+      symptom1: '',
+      symptom2: '',
+      med1: '',
+      med2: '',
+      med3: '',
+      uid: user.getUser()
+  };
+  return userHistory;
+}
+var historyID = "";
+
+function createUserObj(patient) {
   let userObj = {
       name: '',
       email: '',
@@ -27,7 +129,7 @@ $("#login").click(function() {
   .then((result) => {
       user.setUser(result.user.uid);
       $("#login").hide();
-   $("#log-out").show();
+      $("#log-out").show();
       $("#userPic").removeClass("no-user").html(`<img src="${result.user.photoURL}" alt="${result.user.displayName} photo from Google" class="profPic rounded-circle" width="50">`);
       sendToFirebase();
   });
@@ -39,15 +141,18 @@ $("#login").click(function() {
 
 function sendToFirebase() {
   let userBuilder = createUserObj();
+  let historyBuilder = createUserHistory();
   db.addUserFB(userBuilder);
+  db.addUserHistoryFB(historyBuilder);
+  console.log("historyBuilder", historyBuilder);
 }
 
 $("#log-out").click(function(){
   user.googleLogOut();
   // $("#login").removeClass("is-hidden");
   // $("#log-out").addClass("is-hidden");
-  $("#log-out").hide();
-  $("#login").show();
+      $("#log-out").hide();
+      $("#login").show();
 });
 
 
